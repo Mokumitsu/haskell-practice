2b1a124d776b3607cf9afe9b5f4b878a327442a2
Mokumitsu <onodera.ren@heartsoft.biz>
2019-07-31 22:28:22 +0900

    add IOPractice

---
+++ basic_IO/IOPractice.hs
@@ -0,0 +1,32 @@
+-- CL引数を受け取るときに使うらしい
+import System.Environment(getArgs)
+
+main = do
+    -- ******
+    -- * CL引数
+    -- ******
+    -- 引数を取得する
+    -- 配列で取得するのか？？？
+    args <- getArgs
+    -- show hoge で文字列認識させる
+    -- 文字列に対してやると[""]込みで表示
+    -- argsそのまま出してるのでたぶん配列で認識されてる
+    putStrLn (show args)
+    -- putStrLn (show hoge)と同じ動き
+    print args
+    -- argsそのままなので文字列で認識されてしまう
+    -- 配列は今度やる
+    -- putStrLn args
+
+    -- ******
+    -- * I/O
+    -- ******
+    putStrLn "getLine:"
+    -- 標準入力
+    a <- getLine
+    putStrLn a
+
+    putStrLn "getContents"
+    -- (繰返)標準入力
+    b <- getContents
+    putStrLn b
