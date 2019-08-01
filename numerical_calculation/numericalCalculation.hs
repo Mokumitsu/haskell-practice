65e41272874f8a101ed86ba4710c4083674ec08c
Mokumitsu <onodera.ren@heartsoft.biz>
2019-08-01 22:21:20 +0900

    数値計算練習追加

---
+++ numerical_calculation/numericalCalculation.hs
@@ -0,0 +1,16 @@
+main = do
+    let a = 2
+    let b = 3
+    print $ a + b
+    print $ a - b
+    print $ a * b
+    --エラー吐かせない方法がいまいちわからない
+    --print $ (a::Doulbe) / (b::Double)
+    print $ mod a b
+
+    {- 型がお互いにキャストできない場合エラーになる？？？
+    let c=2.0::Double
+    let d=3::Int
+    print $ c + b
+    -}
+
