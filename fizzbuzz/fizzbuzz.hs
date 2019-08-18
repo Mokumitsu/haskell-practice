04f013723b7c838e0d1f4d3a3b3695ff830b563e
Mokumitsu <onodera.ren@heartsoft.biz>
2019-08-18 23:30:17 +0900

    FizzBuzz実装

---
+++ fizzbuzz/fizzbuzz.hs
@@ -0,0 +1,34 @@
+main = do
+    putStrLn "getInt:"
+    a <- getLine
+    let num = read a :: Int
+    let array = [1..num]
+    answer array
+
+answer array = do
+    print str
+    answer next
+    where
+        num = last array
+        -- num = head array
+        next = init array
+        -- next = tail array
+        str = fizzBuzz num
+
+fizzBuzz num = "num:" ++(show num)++ ":" ++ (judge num)
+
+judge num = 
+    if fizzbuzz == 0
+        then "FizzBuzz"
+        else
+        if fizz == 0
+            then "fizz"
+            else
+            if buzz == 0
+                then "buzz"
+                else ""
+     where
+        fizz = mod num 3
+        buzz = mod num 5
+        fizzbuzz = mod num 15
+
